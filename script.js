075ba5dc5a8c6d8c7e4fef9362bed64ed70df7e6
Shweta Mishra <shmis2004@gmail.com>
2022-09-05 19:15:33 +0530

    developers skills

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+/*"use strict";
 
 const x = 23;
 if (x === 23) console.log("hello");
@@ -6,3 +6,19 @@ if (x === 23) console.log("hello");
 const calcAge = (birthYear) => 2022 - birthYear;
 
 console.log("hello WORLD");
+*/
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: Number(prompt(`Degree Celsius`)),
+  };
+  console.log(measurement);
+  console.log(measurement.value);
+  //console.warn(measurement.value);
+  //console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
